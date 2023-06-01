b64f0a09770fb2b1a9bc56a3ecfb1302e2e0c471
jperez-2018495 <jperez-2018495@kinal.edu.gt>
2023-05-31 23:34:52 -0600

    Se agrego crud de entidad "Favorites"

--- sistemaBancario (back)/configs/app.js
+++ sistemaBancario (back)/configs/app.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 3500;
 
 // Routes
+const favoriteRoutes = require('../src/favorites/favorites.routes');
 
 //CONFIGURAR EL SERVIDOR HTTP DE EXPRESS
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +22,7 @@ app.use(helmet());
 app.use(morgan('dev'));
 
 // Ruta
-
+app.use('/favorite', favoriteRoutes);
 
 //Función donde se levanta el servidor
 exports.initServer = () => {
