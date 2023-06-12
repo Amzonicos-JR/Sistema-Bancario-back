11ce3c4379af411da3b9bd869cda7f18cde60e30
Job <jsis-2018520@kinal.edu.gt>
2023-06-12 08:54:40 -0600

    se agrego el crud de la entidad products

---
+++ sistemaBancario (back)/src/products/products.model.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const productsSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    stock: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    total: {
+        type: Number,
+        require: true,
+        default: 0
+    }
+},{
+    versionKey: false,
+})
+
+module.exports = mongoose.model('Products', productsSchema)
