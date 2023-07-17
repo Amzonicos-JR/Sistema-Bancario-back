fa09a6e43c4b6dc1865a8ffcef00b42d4ce3de2f
Job <jsis-2018520@kinal.edu.gt>
2023-07-16 21:51:31 -0600

    Se agrego la funcion get history by user

--- sistemaBancario (back)/src/products/products.routes.js
+++ sistemaBancario (back)/src/products/products.routes.js
@@ -3,10 +3,12 @@
 const productsController = require('./products.controller')
 const express = require('express')
 const api = express.Router()
+const { ensureAuth } = require('../services/authenticated')
 
 api.get('/test', productsController.test)
 api.get('/get', productsController.get)
 api.get('/get/:id', productsController.getProduct)
+api.get('/gethistory', [ensureAuth], productsController.getHistory)
 api.post('/add', productsController.add)
 api.put('/update/:id', productsController.update)
 api.delete('/delete/:id', productsController.delete)
